6880c9b620a7120135bc8d67908777b9e2ec077b
sowmik-sec <sowmik.sec@gmail.com>
2024-06-24 10:54:01 +0600

    Load Dynamic Data, API Call UseEffect Integrate State

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Counter from "./Counter";
+import Friends from "./Friends";
 import Team from "./Team";
+import Users from "./Users";
 
 function App() {
   function handleClick() {
@@ -15,6 +17,8 @@ function App() {
   return (
     <>
       <h3>React Core Concepts 2</h3>
+      <Friends />
+      <Users />
       <Team />
       <Counter />
       <button onClick={handleClick}>Click Me</button>
